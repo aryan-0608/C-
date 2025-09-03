3f1ff6841740f70bd57232e29a64f99d687a966e
Aryan Patel <ap9384907>
2025-09-03 09:24:41 -0700

    comments in cpp

---
+++ 3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//this the program was created by code with aryan
+/*this
+is
+a
+multiline
+comment*/
+int main(){
+    int sum = 6;
+    cout<< "Hello word"<< sum;
+    
+    return 0;
+}
