5347d6ded5fbdd2f914f98ab638ebc06d62a9de5
Aryan Patel <ap9384907>
2025-09-06 02:29:31 -0700

    control Structure

--- 9.cpp
+++ 9.cpp
@@ -7,20 +7,40 @@ int main(){
     int age;
     cout<<"Tell me your age"<<endl;
     cin>>age;
-    if((age<18)&& (age>0)){
-        cout<<"you can not come to my party"<<endl;
-    }
-    else if(age==18){
-        cout<<"you are a kind and yiur will get a kind pass to the party"<<endl;
 
-    }
-    else if(age<1){
-        cout<<"you are not yet born"<<endl;
+   // selection control structure: if else -if else ladder
+    // if((age<18)&& (age>0)){
+    //     cout<<"you can not come to my party"<<endl;
+    // }
+    // else if(age==18){
+    //     cout<<"you are a kind and yiur will get a kind pass to the party"<<endl;
 
+    // }
+    // else if(age<1){
+    //     cout<<"you are not yet born"<<endl;
+
+    // }
+    // else{
+    //     cout<<"the can come to the party"<<endl;
+
+    // }
+
+    // selection control structure: switch case statement
+    switch (age)
+    {
+        case 18:
+        cout<<"you are 18"<<endl;
+        break;
+        case 22:
+        cout<<"you are 22"<<endl;
+        case 2:
+        cout<<"you are 2"<<endl;
+        break;
+
+        default:
+        cout<<"No special cases"<<endl;
+        break;
     }
-    else{
-        cout<<"the can come to the party"<<endl;
-        
-    }
+    cout<<"Done with switch case";
     return 0;
 }
