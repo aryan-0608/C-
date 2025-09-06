b95f167da6306fa8f6e945444cf32a68f7c15077
Aryan Patel <ap9384907>
2025-09-06 01:50:49 -0700

    constants,Manipulators & operator

---
+++ 8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //int a = 34;
+    //cout<<"tje value of a was :"<<a;
+    //a = 45;
+    //cout<<"the value of a is :"<<a;
+    //constant in C++
+
+    float a = 3.11;
+    cout<<"the value of a was:"<<a<<endl;
+    a = 45;// you will get an error because a is  a constant
+    cout<<"the value of a is :"<<a<<endl;
+    return 0;
+}
