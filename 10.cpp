41b9aab4df93e0788981b22990cc541e7ec30604
Aryan Patel <ap9384907>
2025-09-10 09:48:25 -0700

    for loop and while loop

---
+++ 10.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    /* Loop in C++;
+    There are three types of loop in C++:
+    1.For Loop
+    2.while Loop
+    3.Do while Loop
+    */
+
+    /* for loop in C++;*/
+    //int i =1;
+    //cout<<i;
+    // i++;
+
+    //syantax for and for loop
+    //for(initialization ; condition; updation)
+     //{
+        //loop body(C++ code);
+     //}
+
+
+    // for (int i = 1; i < 40; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    //example of  infinite loop
+    // for(int i=1; 34<=40; i++)
+    // {
+    //     cout<<i<<endl;
+    // }
+
+    /*While loop in C++*/
+    //syantax:
+    //while(condition);
+   // {
+       // C++ statements;
+   // }
+
+   //printing 1 to 40 using while loop
+//    int i = 10;
+//    while(i<=40){
+//     cout<<i<<endl;
+//     i++;
+//    }
+//     return 0;
+
+//example of infinate while loop
+int i =1;
+while(true){
+    cout<<i<<endl;
+    i++;
+}
+}
