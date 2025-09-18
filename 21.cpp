173b930ff6ef5178c35083d8ec8977c0d4b852a9
Aryan Patel <ap9384907>
2025-09-18 10:15:22 -0700

    main

---
+++ 21.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+private:
+    int a, b, c;
+
+public:
+    int d, e;
+    void setData(int a1, int b1, int c1); // Declaration
+    void getData()
+    {
+        cout << "the value of a is " << a << endl;
+        cout << "the value of b is " << b << endl;
+        cout << "the value of c is " << c << endl;
+        cout << "the value of d is " << d << endl;
+        cout << "the value of e is " << e << endl;
+    }
+};
+
+void Employee ::setData(int a1, int b1, int c1)
+{
+    a = a1;
+    b = b1;
+    c = c1;
+}
+
+int main()
+{
+    Employee aryan;
+     aryan.d = 34;
+     aryan.e =89;
+     aryan.setData(1,2,4);
+     aryan.getData();
+
+     return 0;
+}
