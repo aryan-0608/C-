3d9771530ea29a1a674db569d9c45b4c9f0d6ced
Aryan Patel <ap9384907>
2025-09-25 10:53:29 -0700

    30.b Example

---
+++ 30b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class point{
+    int x, y;
+    public:
+    point(int a,int b){
+        x = a;
+        y = b;
+        
+    }
+    void displaypoint(){
+        cout<<"The point is ("<<x<<","<<y<<")"<<endl;
+    }
+};
+int main(){
+    point p(1,5);
+    p.displaypoint();
+
+    point q(5, 6);
+    q.displaypoint();
+    return 0;
+}
