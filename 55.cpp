3d5fd0b69d276aa312e83633e3185ce7ce3904ca
Aryan Patel <ap9384907>
2025-10-30 22:37:43 +0530

    Pointer to Derived class in C++

---
+++ 55.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class BaseClass{
+    public:
+    int var_base;
+    void display(){
+        cout<<"Displaying base clas varible var_base"<<var_base<<endl;
+    }
+
+};
+
+class DerivedClass : public BaseClass{
+    public:
+    int var_derived;
+    void display(){
+        cout<<"Displaying Base class variable var_derived "<<var_base<<endl;
+        cout<<"Displaying Derived class variable var_derived "<<var_derived<<endl;
+        
+    }
+
+};
+int main(){
+    BaseClass * base_class_pointer;
+    BaseClass obj_base;
+    DerivedClass obj_derived;
+    base_class_pointer = &obj_derived;
+
+    base_class_pointer->var_base = 34;
+   base_class_pointer->display();
+
+   base_class_pointer->var_base = 384;
+   base_class_pointer->display();
+    return 0;
+}
