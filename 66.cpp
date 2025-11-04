9fbda4d4d5360a780dac5b124befdc4f8aa663e0
Aryan Patel <ap9384907>
2025-11-04 13:00:48 +0530

    Template : class Template with default Parameters|

---
+++ 66.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+template <class T1 = int, class T2=float, class T3=char>
+class Aryan{
+    public:
+    T1 a;
+    T2 b;
+    T3 c;
+    Aryan(T1 x, T2 y, T3 z){
+        a = x;
+        b = y;
+        c = z;
+
+    }
+    void display(){
+        cout<<"The value of a is "<<a<<endl;
+        cout<<"The value of b is "<<b<<endl;
+        cout<<"The value of c is "<<c<<endl;
+    }
+};
+int main(){
+    Aryan <> A(4, 6.4, 'c');
+    A.display();
+    cout<<endl;
+    Aryan<float,char,char> g(1.4,'o','c');
+    g.display();
+    
+    return 0;
+}
